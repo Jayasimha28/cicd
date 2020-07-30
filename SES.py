d963bf0b26279127783abc256cdfea42812c2fe1
Jayasimha28 <cherlopallijayasimha@gmail.com>
2020-07-30 12:00:02 +0530

    Update SES.py

--- SES.py
+++ SES.py
@@ -4,27 +4,29 @@ ses = boto3.client('ses')
 
 body = """
 Hello, This is from Lambda, implemented by Jay
-
 Thanks & Regards,
 Jayasimha
 """
 
-ses.sendemail_email(
-	Source = 'jayasimha.cherlopalli@cognizant.com',
+ses.send_email(
+	Source = 'cherlopallijayasimha@gmail.com',
 	Destination={
 		'ToAddresses': [
-			'jayasimha.cherlopalli@cognizant.com',
-			'Barkha.Singh@cognizant.com'
+			# 'jayasimha.cherlopalli@cognizant.com',
+            'cherlopallijayasimha@gmail.com'
+			# 'Barkha.Singh@cognizant.com'
 		]
 	},
 	Message={
 		'Subject':{
 			'Data' : 'SES DEMO By Lambda',
-			'Charset' : 'UFT-8'
+			'Charset' : 'UTF-8'
 		},
-		'Body'={
-			'Data' : body,
-			'Charset' : 'UFT-8'
-		}
+		'Body':{
+            'Text':{
+                'Data' : body,
+                'Charset' : 'UTF-8'
+            }
+        }
 	}
 )
